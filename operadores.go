18a63c6a73842de9ee25bf966091296ca4123131
Santiago F L <50991702+SantiFL@users.noreply.github.com>
2020-05-04 11:38:41 -0300

    Add files via upload

---
+++ operadores.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numero1 float32 = 10
+	var numero2 float32 = 7
+
+	fmt.Print("la suma es ")
+	fmt.Println(numero1 + numero2)
+	fmt.Print("la resta es")
+	fmt.Println(numero1 - numero2)
+	fmt.Println("la multiplicacion es ")
+	fmt.Println(numero1 + numero2)
+	fmt.Println("la divicion es ")
+	fmt.Print(numero1 / numero2)
+
+}
